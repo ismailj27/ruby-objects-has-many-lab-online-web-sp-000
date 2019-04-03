addabfdee40fc84da71554ab83db737cb5b06c5c
Ismail Jalloh <ijalloh21@gmail.com>
2019-04-03 02:52:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -23,8 +23,21 @@ class Author
     @@post_counter+=1
   end
   
-  def add_post_by_title
+  def add_post_by_title(a_title)
+    a_post = Post.new(a_title)
+    @posts << a_post
+    a_post.author = self
+    @@post_counter+=1
+  end
     
     
-    
+  
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_counter+=1
+  end
+  
 end
